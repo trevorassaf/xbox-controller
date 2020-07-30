0369eab41034c95ac4e6ea0ab2f6b221e14dabcd
Trevor Assaf <bozkurtus@google.com>
2020-07-29 22:48:29 -0700

    Add slight timeout

--- src/joystick_input_to_servo_action_mapper.cpp
+++ src/joystick_input_to_servo_action_mapper.cpp
@@ -264,7 +264,7 @@ bool JoystickInputToServoActionMapper::StopServoMovement()
     return false;
   }
 
-  //std::this_thread::sleep_for(std::chrono::milliseconds{50});
+  std::this_thread::sleep_for(std::chrono::milliseconds{5});
 
   /*
   uint16_t stopped_position_;
